a133b965ea5449be5a9209219a7379cde99f513e
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-28 17:31:16 +0300

    add nav items and links

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+// var navElements = document.querySelectorAll(".navbar .container .nav-items ul li a"); 
+
+
+// for(var i = 0 ; i < navElements.length ; i++){
+//     navElements[i].addEventListener("mouseover" , function(e){
+
+//         console.log(e.target);
+//         e.target.classList.add("a-new-style");
+//         e.target.classList.remove("text-white");
+//     })
+// }
+
