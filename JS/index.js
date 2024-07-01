851c35456f0765ae588f35a3f83cf08b00e0304b
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-07-01 05:22:31 +0300

    form of contact page done

--- JS/index.js
+++ JS/index.js
@@ -8,121 +8,38 @@ var displaySection = document.querySelector("main section.display .row");
   // var request = await fetch("http://api.weatherapi.com/v1/search.json?key=51d6791ef4a44063915235132242806&q=lond");
   //will return location and current
 
-console.log(displaySection)
+  // var lat , long ; 
 
-
-
-async function getWeather(countryName) {
-  try {
-    var request2 = await fetch(`http://api.weatherapi.com/v1/current.json?key=51d6791ef4a44063915235132242806&q=${countryName}`);
-    var data2 = await request2.json();
-    console.log(data2); 
+  function gotLocation(position){
+    console.log(position)
   }
+
+  function getLoctaion(){
+    var req= fetch(`http://api.weatherapi.com/v1/current.json?key=51d6791ef4a44063915235132242806&q=${long},${at}`); 
+    var data = req.json(); 
+    
+  }
+
+    searchBtn.addEventListener("click" ,   async()=>{
+    await navigator.geolocation.getCurrentPosition(gotLocation);
+
+    }
+)
+
+
+// async function myInfo(lat , long){
+   
+//   var myLoc =await fetch(`http://api.weatherapi.com/v1/lon.json.json?key=51d6791ef4a44063915235132242806&q=${lat},${long}`); 
   
-  catch (error) {
-    console.log("sss");
-  }
-
-  return data2 ; 
-}
-
-
-countryInput.addEventListener("input", async function () {
-    var countryName = countryInput.value;
-    try{
-   var data =  await getWeather(countryName);
-      console.log(data.location.name);
-      display(data) ; 
-
-}
-catch(e){
-    console.log("sad");
-
-}
-
-});
-
-
-// current
+//     var data =await myLoc.json() ; 
+   
+//     console.log("long ", data);
+// }
+  
+  // myLocation() ;
+  // myInfo(lat , long)
 
 
 
-function display(x) {
-
-
-  displaySection.innerHTML =  `<  <!-- ="===========================""" -->
-        <div class="col-lg-4 ">
-          <div class="item">
-        <div class="card w-100 pb-4" style="width: 18rem;">
-                       <div class="card-header d-flex justify-content-between" >
-                         <p>Saturday</p>
-                         <p>29 June</p>
-                   
-                        </div>
-     
-                      <div class="p-4">
-                       <p>${x.location.name}</p>
-                       <h4 class="fs-1">${x.current.temp_c}°C</h4>
-                    <img src=${x.current.condition.icon}>
-                   <p>${x.current.condition.text}</p>
-                      </div>
-                       <div class="d-flex">
-                         <div>
-                           <img src="../images/icon-umberella.png" alt="umberella">
-                           <span>${x.current.temp_f}%</span>
-                         </div>
-                         <div>
-                           <img src="../images/icon-wind.png" alt="umberella">
-                           <span>${x.current.wind_kph}Km/h</span>
-                         </div>
-                         <div>
-                           <img src="../images/icon-compass.png" alt="umberella">
-                           <span>${x.current.wind_dir}</span>
-                         </div>
-                    
-                         </div>
-
-        </div>
-
-
-
-       </div>
-        </div>
-        <!-- --------------------------------------- -->
-        <div class="col-lg-4 ">
-          <div class="item">
-            <div class="card w-100 pb-5" style="width: 18rem;">
-              <div class="card-header" >
-                <p class="text-center">🇸unday</p>
-              </div>
-             <div class="p-4 text-center">
-              <p>Alex</p>
-              <h4 class="fs-1">27.0*C</h4>
-              <i>Icon</i>
-              <p>clear</p>
-              
-          </div>
-        </div>
-          </div>
-      </div>
-<!-- ="=======================================================""" -->
-      <div class="col-lg-4">
-        <div class="item">
-          <div class="card w-100 pb-5" style="width: 18rem;">
-            <div class="card-header" >
-              <p class="text-center">🇸unday</p>
-            </div>
-           <div class="p-4 text-center">
-            <p>Alex</p>
-            <h4 class="fs-1">27.0*C</h4>
-            <i>Icon</i>
-            <p>clear</p>
-            
-        </div>
-      </div>
-        </div>
-    </div>
-
-          `;
-  }
+  
 
